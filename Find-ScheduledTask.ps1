e8ba2274286f527477e392ccb49a259f5fb9fa7d
Sophie N <55649555+sophxe@users.noreply.github.com>
2025-02-12 18:33:36 +0000

    Add files via upload

---
+++ Find-ScheduledTask.ps1
@@ -0,0 +1,51 @@
+<#
+.SYNOPSIS
+Find-ScheduledTask aims to find a scheduled task on the host with a given task name with the -Triage switch, and when passed with the Remediation switch, can remediate it from the host
+.PARAMETER name
+The name of the task to search for.
+.EXAMPLE 
+Find-ScheduledTask.ps1 -Name 'malservice' -Triage
+Find-ScheduledTask.ps1 -Name 'malservice' -Remediate
+#>
+
+[CmdletBinding()]
+param (
+    [string[]]$Name,
+    [switch]$Triage,
+    [switch]$Remediate
+)
+
+function Find-ScheduledTask {
+    # Get scheduled tasks
+    $tasks = cmd.exe /c schtasks /query /fo csv /v | ConvertFrom-Csv | Select-Object TaskName, "Task To Run" | Where-Object {$_.TaskName -like "*$Name*"}
+
+    # Print out scheduled tasks
+    Write-Host "`n=============================`n Scheduled Tasks Found`n============================="
+    Write-Output $tasks | Format-List
+
+    if ($Triage) {
+        $count = $tasks.length
+        Write-Output "Triage Completed - $count Scheduled Tasks Found. To remediate, re-run script with -Remediate switch"
+    }
+
+    # To remove the tasks
+    if ($Remediate) {
+        foreach ($task in $tasks) {
+            try {
+                $taskName = $task.TaskName
+                $taskToRun = $task."Task To Run"
+                cmd.exe /c schtasks /tn $taskName /delete /f
+                Write-Output "[-] Task $taskName with action $taskToRun deleted"
+            }
+            catch {
+                # If it fails
+                Write-Output "Cannot delete $taskName"
+            }
+        }
+        
+    }
+}
+
+Find-ScheduledTask
+
+
